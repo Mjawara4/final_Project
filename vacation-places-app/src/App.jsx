f8ffa60505e37b4d0a0b91185eab73f3abc13e9d
Mjawara4 <101847266+Mjawara4@users.noreply.github.com>
2023-12-23 01:01:14 -0500

    build client

--- vacation-places-app/src/App.jsx
+++ vacation-places-app/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [places, setPlaces] = useState([]);
+  const [newPlace, setNewPlace] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/places')
+      .then(response => setPlaces(response.data));
+  }, []);
+
+  const addPlace = () => {
+    axios.post('http://localhost:3001/api/places', { name: newPlace })
+      .then(response => setPlaces([...places, response.data]));
+    setNewPlace('');
+  };
 
   return (
-    <>
+    <div>
+      <h1>Vacation Places</h1>
+      <ul>
+        {places.map(place => (
+          <li key={place.name}>{place.name}</li>
+        ))}
+      </ul>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" value={newPlace} onChange={(e) => setNewPlace(e.target.value)} />
+        <button onClick={addPlace}>Add Place</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
